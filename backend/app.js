4bd62b0fa5e735240fcec041c3c1200c6c44149c
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-02-09 17:28:26 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,10 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(fileUpload());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
    res.setHeader("Access-Control-Allow-Methods", "*");
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+  
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
