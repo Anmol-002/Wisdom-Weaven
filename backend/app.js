fd5aee4be03b169819cc222d7281855029776629
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-02-09 23:31:04 +0530

    Update app.js-4

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,9 @@ app.use(fileUpload());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://wisdom-weaven.netlify.app");
-   res.setHeader("Access-Control-Allow-Methods", "*");
+   res.setHeader("Access-Control-Allow-Methods", ['GET','POST','PUT','DELETE','PATCH']);
    res.setHeader("Access-Control-Allow-Credentials", "true");
+   res.setHeader("Content-Type", "application/json");
   
   res.header(
     "Access-Control-Allow-Headers",
