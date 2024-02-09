66616290e260b4024d9adf8ef850fb20358dd4af
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-02-09 16:48:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,8 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(fileUpload());
-
-
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
- 
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -37,12 +32,6 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
 
 app.use(errorMiddleware);
 
