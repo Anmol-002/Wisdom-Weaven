51c4c65da778e44c87381ebdbf03943107092afc
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-02-09 23:19:06 +0530

    Update app.js-2

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(fileUpload());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://65c66471879b1f2244eba9c6--charming-crepe-a0e39e.netlify.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
    res.setHeader("Access-Control-Allow-Methods", "*");
    res.setHeader("Access-Control-Allow-Credentials", "true");
   
